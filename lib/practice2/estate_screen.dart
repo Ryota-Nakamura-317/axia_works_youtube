4d505fb2510cfd7ad66e491acac565e6c4006663
中村遼太 <nkmr94317rt@gmail.com>
2021-04-05 19:07:54 +0900

    [update] bodyの切り出し、ダミーデータの作成

--- lib/practice2/estate_screen.dart
+++ lib/practice2/estate_screen.dart
@@ -13,158 +13,6 @@ class EstateScreen extends StatelessWidget {
   }
 }
 
-Widget _createFloatingActionButton() {
-  return Container(
-    width: 75,
-    height: 75,
-    child: FittedBox(
-      child: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.teal[400],
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(Icons.search),
-            Text(
-              '物件',
-              style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
-}
-
-Widget _createBottomNavigationBar() {
-  return BottomNavigationBar(
-    type: BottomNavigationBarType.fixed,
-    showUnselectedLabels: true,
-    unselectedItemColor: Colors.grey[400],
-    selectedItemColor: Colors.teal[400],
-    unselectedFontSize: 12,
-    unselectedIconTheme: IconThemeData(size: 30),
-    selectedIconTheme: IconThemeData(size: 30),
-    items: [
-      BottomNavigationBarItem(
-        icon: Icon(Icons.home),
-        label: 'ホーム',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.favorite_border),
-        label: 'お気に入り',
-      ),
-      BottomNavigationBarItem(
-        icon: Badge(
-          badgeContent: Text('1', style: TextStyle(color: Colors.white)),
-          child: Icon(Icons.message),
-        ),
-        label: 'メッセージ',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.person_outlined),
-        label: 'マイページ',
-      ),
-    ],
-  );
-}
-
-Widget _createBody() {
-  final estateList = estateInfo();
-  return Column(
-    children: [
-      Container(
-        padding: EdgeInsets.symmetric(vertical: 4, horizontal: 4),
-        height: 160,
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                child: Row(
-                  children: [
-                    SizedBox(width: 10),
-                    Text(
-                      'カウルのおすすめ',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      '新着３件',
-                      style: TextStyle(color: Colors.red),
-                    ),
-                    SizedBox(width: 150),
-                    Text(
-                      '編集',
-                      style: TextStyle(color: Colors.teal[400]),
-                    ),
-                    Icon(
-                      Icons.edit,
-                      color: Colors.teal[400],
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 400,
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  color: Colors.brown[50],
-                  child: Container(
-                    padding: EdgeInsets.only(left: 8),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Row(
-                          children: [
-                            Icon(Icons.train, size: 20),
-                            SizedBox(width: 5),
-                            Text('東京駅・品川駅・川崎駅・横浜駅・目黒駅'),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Icon(Icons.monetization_on, size: 20),
-                            SizedBox(width: 5),
-                            Text('下限なし〜2,000万円'),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Icon(Icons.info_outline, size: 20),
-                            SizedBox(width: 5),
-                            Text('1R〜4LDK / 10㎡以上 / 徒歩20分'),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      Expanded(
-        child: ListView.builder(
-          shrinkWrap: true,
-          itemCount: estateList.length,
-          itemBuilder: (context, index) {
-            return _createEstateInfo(context, estateList[index]);
-          },
-        ),
-      )
-    ],
-  );
-}
-
 Widget _createAppBar() {
   return AppBar(
     backgroundColor: Colors.white,
@@ -210,6 +58,163 @@ Widget _createAppBar() {
         ),
       ),
     ],
+  );
+}
+
+Widget _createBody() {
+  final _estateList = estateInfo();
+  final int infoCard = 1;
+  final int detailCard = 2;
+  return ListView.builder(
+    itemCount: _estateList.length,
+    itemBuilder: (context, index) {
+      final data = _estateList[index];
+      if (infoCard == data.cellType) {
+        return _createSearchInfo(context, data.searchData);
+      } else if (detailCard == data.cellType) {
+        return _createEstateInfo(context, data.estateData);
+      } else {
+        return Container();
+      }
+    },
+  );
+}
+
+Widget _createBottomNavigationBar() {
+  return BottomNavigationBar(
+    type: BottomNavigationBarType.fixed,
+    showUnselectedLabels: true,
+    unselectedItemColor: Colors.grey[400],
+    selectedItemColor: Colors.teal[400],
+    unselectedFontSize: 12,
+    unselectedIconTheme: IconThemeData(size: 30),
+    selectedIconTheme: IconThemeData(size: 30),
+    items: [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'ホーム',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.favorite_border),
+        label: 'お気に入り',
+      ),
+      BottomNavigationBarItem(
+        icon: Badge(
+          badgeContent: Text('1', style: TextStyle(color: Colors.white)),
+          child: Icon(Icons.message),
+        ),
+        label: 'メッセージ',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.person_outlined),
+        label: 'マイページ',
+      ),
+    ],
+  );
+}
+
+Widget _createFloatingActionButton() {
+  return Container(
+    width: 75,
+    height: 75,
+    child: FittedBox(
+      child: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.teal[400],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.search),
+            Text(
+              '物件',
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _createSearchInfo(BuildContext context, SearchData searchData) {
+  return Container(
+    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 4),
+    height: 160,
+    child: Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+            child: Row(
+              children: [
+                SizedBox(width: 10),
+                Text(
+                  searchData.title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 10),
+                Text(
+                  '新着${searchData.num}件',
+                  style: TextStyle(color: Colors.red),
+                ),
+                SizedBox(width: 150),
+                Text(
+                  '編集',
+                  style: TextStyle(color: Colors.teal[400]),
+                ),
+                Icon(
+                  Icons.edit,
+                  color: Colors.teal[400],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 400,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              color: Colors.brown[50],
+              child: Container(
+                padding: EdgeInsets.only(left: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.train, size: 20),
+                        SizedBox(width: 5),
+                        Text(searchData.station),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.monetization_on, size: 20),
+                        SizedBox(width: 5),
+                        Text(searchData.price),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.info_outline, size: 20),
+                        SizedBox(width: 5),
+                        Text(searchData.details),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
   );
 }
 
@@ -322,33 +327,70 @@ Widget _createEstateInfo(BuildContext context, EstateData data) {
   );
 }
 
-List<EstateData> estateInfo() {
+List<ToggleShowData> estateInfo() {
   return [
-    EstateData(
-      'images/スクリーンショット 2021-04-05 午後13.55.32 午後.png',
-      'Rising place 川崎',
-      '2,000万円',
-      '京急本線 京急川崎駅 より 徒歩9分',
-      '1K / 21.24㎡ 南西向き',
-      '2階/15階建 築5年',
+    ToggleShowData(
+      1,
+      searchData: SearchData(
+        'カウルのおすすめ',
+        3,
+        '東京駅・品川駅・川崎駅・横浜駅・目黒駅',
+        '下限なし〜2,000万円',
+        '1R〜4LDK / 10㎡以上 / 徒歩20分',
+      ),
     ),
-    EstateData(
-      'images/スクリーンショット 2021-04-05 午後13.55.32 午後.png',
-      'Rising place 川崎',
-      '2,000万円',
-      '京急本線 京急川崎駅 より 徒歩9分',
-      '1K / 21.24㎡ 南西向き',
-      '2階/15階建 築5年',
+    ToggleShowData(
+      2,
+      estateData: EstateData(
+        'images/スクリーンショット 2021-04-05 午後13.55.32 午後.png',
+        'Rising place 川崎',
+        '2,000万円',
+        '京急本線 京急川崎駅 より 徒歩9分',
+        '1K / 21.24㎡ 南西向き',
+        '2階/15階建 築5年',
+      ),
     ),
-    EstateData(
-      'images/スクリーンショット 2021-04-05 午後13.55.32 午後.png',
-      'Rising place 川崎',
-      '2,000万円',
-      '京急本線 京急川崎駅 より 徒歩9分',
-      '1K / 21.24㎡ 南西向き',
-      '2階/15階建 築5年',
+    ToggleShowData(
+      2,
+      estateData: EstateData(
+        'images/スクリーンショット 2021-04-05 午後13.55.32 午後.png',
+        'Rising place 川崎',
+        '2,000万円',
+        '京急本線 京急川崎駅 より 徒歩9分',
+        '1K / 21.24㎡ 南西向き',
+        '2階/15階建 築5年',
+      ),
+    ),
+    ToggleShowData(
+      2,
+      estateData: EstateData(
+        'images/スクリーンショット 2021-04-05 午後13.55.32 午後.png',
+        'Rising place 川崎',
+        '2,000万円',
+        '京急本線 京急川崎駅 より 徒歩9分',
+        '1K / 21.24㎡ 南西向き',
+        '2階/15階建 築5年',
+      ),
     ),
   ];
+}
+
+class ToggleShowData {
+  final int cellType;
+  final EstateData estateData;
+  final SearchData searchData;
+
+  ToggleShowData(this.cellType, {this.estateData, this.searchData});
+}
+
+class SearchData {
+  final String title;
+  final int num;
+  final String station;
+  final String price;
+  final String details;
+
+  SearchData(this.title, this.num, this.station, this.price, this.details);
 }
 
 class EstateData {
@@ -360,11 +402,5 @@ class EstateData {
   final spec;
 
   EstateData(
-    this.image,
-    this.name,
-    this.price,
-    this.distance,
-    this.large,
-    this.spec,
-  );
+      this.image, this.name, this.price, this.distance, this.large, this.spec);
 }
