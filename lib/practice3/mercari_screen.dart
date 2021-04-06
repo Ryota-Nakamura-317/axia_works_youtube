11a9f29b17c758c9ba7ef5c0862691ba1da7c45d
中村遼太 <nkmr94317rt@gmail.com>
2021-04-06 10:58:44 +0900

    no message

--- lib/practice3/mercari_screen.dart
+++ lib/practice3/mercari_screen.dart
@@ -3,6 +3,20 @@ import 'package:flutter/material.dart';
 class MercariScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: _createAppBar(),
+    );
   }
+}
+
+Widget _createAppBar() {
+  return AppBar(
+    centerTitle: true,
+    title: Text(
+      '出品',
+      style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600),
+    ),
+    iconTheme: IconThemeData(color: Colors.black),
+    backgroundColor: Colors.white,
+  );
 }
