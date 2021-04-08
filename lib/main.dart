a9fa0140eacc2ef981709c7456983d718bf2faa2
中村遼太 <nkmr94317rt@gmail.com>
2021-04-08 11:25:09 +0900

    [add] ファイル作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:axia_works_youtube/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Tutorial',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: HomeScreen(),
     );
   }
